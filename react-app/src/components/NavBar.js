c2e50aa424781a50897bf06126735d4e267eec24
Jimmy Ly <lyjimmy19@gmail.com>
2021-07-02 11:50:04 -0400

    some navbar items will appear based on login and logout

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import {Flex, Spacer, Square, Center, Box} from "@chakra-ui/react"
+import { useSelector } from 'react-redux';
 
 const NavBar = () => {
+  const sessionUser = useSelector(state=> state.session.user)
+
+  let sessionLinks;
+  if(sessionUser){
+    sessionLinks=(
+      <>
+      <Flex justify="space-between" bg="tomato" h="100px" w="100%" color="white" p={6}>
+        <NavLink to="/users" exact={true} activeClassName="active">
+          Users
+        </NavLink>
+        <LogoutButton />
+      </Flex>
+    </>
+    )
+  } else{
+    sessionLinks=(
+      <>
+      <Flex justify="space-between" bg="tomato" h="100px" w="100%" color="white" p={6}>
+        <NavLink to="/" exact={true} activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink to="/login" exact={true} activeClassName="active">
+          Login
+        </NavLink>
+        <NavLink to="/sign-up" exact={true} activeClassName="active">
+          Sign Up
+        </NavLink>
+      </Flex>
+    </>
+    )
+  }
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      {sessionLinks}
     </nav>
   );
 }
